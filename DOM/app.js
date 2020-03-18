ed4ba09d431032a387289a9ee0af433dd3bfcf05
Sierra Barnes <sierrabarnes@Sierras-MacBook-Pro.local>
2020-03-18 17:24:27 -0400

    added a button and text box, and a click event on the button that displays an alert containing the text that is entered in the text box

--- DOM/app.js
+++ DOM/app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function (){
+    // Objective 1. creates a button with text, adds it to the doc, and creates an alert when clicked
     let btn = document.createElement('button');
     let btnText = document.createTextNode('click to make an alert!');
     btn.appendChild(btnText);
@@ -7,4 +8,17 @@ document.addEventListener('DOMContentLoaded', function (){
     btn.addEventListener('click', function (){
         alert('Thanks for clicking!');
     })
+
+    let btn2 = document.createElement('button');
+    let btn2Text = document.createTextNode('submit');
+    let inputBox = document.createElement('input');
+    btn2.appendChild(btn2Text);
+    document.body.appendChild(inputBox);
+    document.body.appendChild(btn2);
+
+    btn2.addEventListener('click', function(){
+        
+        alert(inputBox.value);
+    })
+    
 }) 
