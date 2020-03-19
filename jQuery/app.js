8809ab5c31964bf30c1b2d924e90126586d3f152
Sierra Barnes <sierrabarnes@Sierras-MacBook-Pro.local>
2020-03-19 15:43:46 -0400

    added function to generate random color. added paragraph with text and click event to change the paragraph text to a random color

--- jQuery/app.js
+++ jQuery/app.js
@@ -12,10 +12,27 @@ $(document).ready(function () {
         alert($('#textBox').val());
     })
 
+    // Objective #3. changes div background color to blue if the mouse is hovering over it, then change back when it no longer hovers.
     $('#colorChangeDiv').mouseover(function () {
         $(this).css('background-color', 'blue');
     })
     $('#colorChangeDiv').mouseout(function () {
         $(this).css('background-color', 'white');
+    })
+
+    // Objective #4. creates function for generating random color.
+    function randomColor() {
+        let colorR = Math.floor((Math.random() * 256));
+        let colorG = Math.floor((Math.random() * 256));
+        let colorB = Math.floor((Math.random() * 256));
+        let color = `rgb(${colorR},${colorG},${colorB})`;
+        return color;
+    }
+
+    // Objective #4. creates paragraph with text and appends to body of doc. click function changes text color to a random color.
+    let para = $('<p></p>').text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+    $('body').append(para);
+    $(para).click(function () {
+        $(this).css('color', randomColor());
     })
 })
